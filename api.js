e6c2e472d0c70867a8c5adbf53ff48cec212c88f
Caio-Fernando241 <caio.fernando@sou.unifeob.edu.br>
2025-04-13 20:13:27 -0300

    Add files via upload

---
+++ api.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const routes = require('./routers/routes');
+const errorHandler = require('./middlewares/errorHandler');
+
+app.use(express.json());
+app.use('/', routes);
+app.use(errorHandler); // Deve ser o último middleware
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
